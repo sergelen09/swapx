54e2d4de4955811b469ef47dd1f02db640c7a2dd
Danny Nguyen <danny.bn52@gmail.com>
2019-10-30 20:18:34 -0400

    Styled Index

--- app/javascript/react/components/items/WelcomeTile.js
+++ app/javascript/react/components/items/WelcomeTile.js
@@ -3,9 +3,11 @@ import React from 'react'
 const WelcomeTile = props => {
   return(
     <div className="jumbotron text-center">
-      <h1 className="display-4">Hello, world!</h1>
-      <hr className="my-4"/>
-      <button type="button" className="btn btn-primary sign-up-btn">Sign Up</button>
+      <div className='content'>
+        <h1 className="display-4">Hello, world!</h1>
+        <hr className="my-4"/>
+        <button type="button" className="btn btn-primary sign-up-btn">Sign Up</button>
+      </div>
     </div>
   )
 }
